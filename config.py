007bca5658504023a42178d1f7afa70dc480ad6a
ZENCURSE <manibharathi21062@gmail.com>
2026-09-06 17:16:54 +0530

    Rename DB_URI to DB_URL in config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6426143861"))
 PORT = os.environ.get("PORT", "8080")
 
 # Database
-DB_URI = os.environ.get("DB_URI", "")
+DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 #Auto approve 
